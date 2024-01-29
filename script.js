8c50b11d48953fab42ffea2bdf7ea8a0bb27f76e
Chris <148640978+ChrisUK3069@users.noreply.github.com>
2024-01-29 22:16:25 +0000

    Course requirements met, further polishing of CSS/extraneous code required.

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ let operand = "";
 let displayValue;
 let screenValue = "";
 let sum = "";
+dbZero = false;
+
+function divideByZero() {
+    if (operand == "/" && primaryValue === 0) {
+        console.log("Cannot Divide By Zero");
+        Display.textContent = "";
+        Display.textContent = "Cannot Divide By Zero!";
+        let primaryValue = "";
+        let secondaryValue = "";
+        let operand = "";
+        let displayValue;
+        let screenValue = "";
+        let sum = "";
+        dbZero = true;
+
+    }
+}
 
 function add(number1, number2) {
     console.log("function add:" + number1, number2);
@@ -123,6 +140,7 @@ btnAC.addEventListener('click', function (e) {
     screenValue = "";
     operand = "";
     sum = "";
+    dbZero = false;
     displayOperand.textContent = "";
 
 });
@@ -229,45 +247,61 @@ btnDivide.addEventListener('click', function (e) {
     displayOperand.textContent = operand;
 
     primaryValue = parseFloat(primaryValue);
-    if (secondaryValue === "") {    // To run on first entry
-        secondaryValue = primaryValue;
-        primaryValue = "";
-    } else {                        // To run when both variables contain values
+    divideByZero();
+    if (dbZero === true) {
+        console.log("Error, Cannot Divide by Zero, all values reset.");
+    } else {
+
+        if (secondaryValue === "") {    // To run on first entry
+            secondaryValue = primaryValue;
+           primaryValue = "";
+        } else {                        // To run when both variables contain values
         
-        if (sum === "") {
-            sum = operate(operand, secondaryValue, primaryValue);
-            console.log("function btnAdd, both values full, empty to full sum" + sum);
-            screenUpdate(sum);
-            primaryValue = "";
-            // secondaryValue = "";
-        } else {
-            console.log("function btnAdd, both values full, pre operate() call" + sum)
-            sum = operate(operand, sum, primaryValue);
-            sum = Math.round(sum * 10) / 10;
+          if (sum === "") {
+              sum = operate(operand, secondaryValue, primaryValue);
+             console.log("function btnAdd, both values full, empty to full sum" + sum);
+             screenUpdate(sum);
+             primaryValue = "";
+             // secondaryValue = "";
+          } else {
+                console.log("function btnAdd, both values full, pre operate() call" + sum)
+             sum = operate(operand, sum, primaryValue);
+             sum = Math.round(sum * 10) / 10;
             
-            console.log("function btnAdd, both values full, post operate() call" + sum)
-            screenUpdate(sum);
-            primaryValue = "";
+              console.log("function btnAdd, both values full, post operate() call" + sum)
+              screenUpdate(sum);
+                primaryValue = "";
+            }
         }
     }
 });
 
 const btnEqual = document.querySelector("#equ");
 btnEqual.addEventListener('click', function (e) {
-
+    
     primaryValue = parseFloat(primaryValue);
-    if (secondaryValue === "") {    // no other value to operate on
+    divideByZero();
+
+
+
+    if (dbZero === true) {
+        console.log("Error, Cannot Divide by Zero, all values reset.");
+    } else {
+
+  
+        if (secondaryValue === "") {    // no other value to operate on
         displayOperand.textContent = "";
         operand = "=";
         displayOperand.textContent = operand;
-    } else {  
+        } else {  
         sum = operate(operand, secondaryValue, primaryValue);
         sum = Math.round(sum * 10) / 10;
         screenUpdate(sum);
         primaryValue = sum;
         secondaryValue = "";
         sum = "";
-        
+            console.log(sum);
+        }
     }
     
 });
